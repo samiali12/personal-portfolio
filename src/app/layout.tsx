c02374895447c9346704e91f6f6d6b0cbbed94ce
Sami Ali <sami_jameel@nextpack.org>
2025-02-09 12:39:31 +0500

    implement the about, projects and contact us section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Ovo, Outfit } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Ovo({
   variable: "--font-ovo",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased leading-8 overflow-x-hidden`}
       >
+        <SpeedInsights />
         <Toaster />
         {children}
       </body>
